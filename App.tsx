7f91d4bdb592988699e587edb83c2b10571ddde1
Lucas Luis Borges <lucasluisborges@outlook.com>
2022-05-05 15:37:09 -0300

    Components base

--- App.tsx
+++ App.tsx
@@ -1,34 +1,38 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
-import { theme } from './src/theme';
-import AppLoading from 'expo-app-loading';
-import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import "react-native-gesture-handler";
 
-import { Widget } from './src/components/Widget';
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import { theme } from "./src/theme";
+
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+
+import Widget from "./src/components/Widget";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
-    Inter_500Medium
-  })
+    Inter_500Medium,
+  });
 
   if (!fontsLoaded) {
-    return <AppLoading/>
+    return <AppLoading />;
   }
 
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: theme.colors.background
-    }}>
-      <Widget />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+      }}
+    >
+      <StatusBar style="light" backgroundColor="transparent" translucent />
 
-      <StatusBar 
-        style="light"
-        backgroundColor='transparent'
-        translucent
-      />
+      <Widget />
     </View>
   );
 }
