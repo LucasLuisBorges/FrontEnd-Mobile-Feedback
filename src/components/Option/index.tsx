9de7afa0bdb205c8913a37bee963e6f79adf55ca
Lucas Luis Borges <lucasluisborges@outlook.com>
2022-05-06 13:37:07 -0300

    corrigir

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -9,10 +9,25 @@ import {
 
 import { styles } from './styles';
 
-export function Option() {
-  return (
-    <TouchableOpacity style={styles.container}>
+interface Props extends TouchableOpacityProps {
+  title: string;
+  image: ImageProps;
+}
 
+export function Option({ title, image, ...rest } : Props) {
+  return (
+    <TouchableOpacity 
+      style={styles.container}
+      {...rest}
+    >
+      <Image
+        source={image}
+        style={styles.image}
+      />
+
+      <Text style={styles.title}>
+        {title}
+      </Text>
     </TouchableOpacity>
   );
 }
